bfc464ee8bedc8aea98c935ed82e09f5fcdc1ac1
Rohan Moore and Tavish OKeefe <rohan@>
2018-10-18 13:39:23 -0700

    add functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,43 @@
 $(document).ready(function() {
+  $("form#triangle").submit(function(event) {
+    event.preventDefault();
+    var sideOne = parseFloat($("#side1").val());
+    var sideTwo = parseFloat($("#side2").val());
+    var sideThree = parseFloat($("#side3").val());
 
+    if(sideOne && sideTwo && sideThree) {
+      if (sideOne >= sideTwo + sideThree || sideTwo >= sideOne + sideThree || sideThree >= sideOne + sideTwo) {
+        $("#not-a-triangle").show();
+        $("#equilateral").hide();
+        $("#isosceles").hide();
+        $("#scalene").hide();
+        $("#invalid").hide();
+      } else if (sideOne === sideTwo && sideOne === sideThree) {
+        $("#not-a-triangle").hide();
+        $("#equilateral").show();
+        $("#isosceles").hide();
+        $("#scalene").hide();
+        $("#invalid").hide();
+      } else if (sideOne === sideTwo || sideOne === sideThree || sideTwo === sideThree){
+        $("#not-a-triangle").hide();
+        $("#equilateral").hide();
+        $("#isosceles").show();
+        $("#scalene").hide();
+        $("#invalid").hide();
+      } else {
+        $("#not-a-triangle").hide();
+        $("#equilateral").hide();
+        $("#isosceles").hide();
+        $("#scalene").show();
+        $("#invalid").hide();
+      }
+
+    } else {
+      $("#not-a-triangle").hide();
+      $("#equilateral").hide();
+      $("#isosceles").hide();
+      $("#scalene").hide();
+      $("#invalid").show();
+    }
+  });
 });
